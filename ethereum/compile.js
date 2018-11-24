061ab4aaee7aa7f2fb558fa99ce2481ed1fd1ff1
Sherry-94 <shaheryar.akram@hotmail.com>
2018-11-24 20:45:25 +0500

    second commit

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -2,6 +2,7 @@ const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 
+// fs = file system
 const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
 
